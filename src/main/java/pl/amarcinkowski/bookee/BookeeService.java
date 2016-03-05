6762aebb1447105171813b7b64e2213d6b81dd02
amarcinkowski <andrzej.marcinkowski@gmail.com>
2016-03-06 00:36:53 +0100

    create code

---
+++ src/main/java/pl/amarcinkowski/bookee/BookeeService.java
@@ -0,0 +1,14 @@
+package pl.amarcinkowski.bookee;
+
+import static spark.Spark.*;
+
+public class BookeeService {
+
+	public static final Integer PORT = 8080;
+	public static final String DOMAIN = "localhost";
+	public static final String API = "books";
+
+	public BookeeService() {
+	}
+
+}
---
+++ src/main/java/pl/amarcinkowski/bookee/Main.java
@@ -0,0 +1,10 @@
+package pl.amarcinkowski.bookee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BookeeService bookeeService = new BookeeService();
+		bookeeService.toString();
+	}
+
+}
